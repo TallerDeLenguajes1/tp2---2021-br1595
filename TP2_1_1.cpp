8a62eb4dc32579b217946ea75be8e51e95fcae10
Bruno Alberto Rongetti Jaime <b.a.r.j.1595@gmail.com>
2021-04-10 15:38:15 -0300

    first commit

---
+++ TP2_1_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define N 4
+#define M 5
+
+void main(){
+    int f,c;
+    double mt[N][M];
+
+    for(f = 0;f<N; f++){
+
+        for(f = 0;f<N; f++){
+    
+            std::cout << mt[f][c];
+        }
+        std::cout << “\n”;
+    }
+
+}
