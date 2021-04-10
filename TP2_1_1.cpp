9195ed739c0b96537b2a5c22533814c6e7fee442
Bruno Alberto Rongetti Jaime <b.a.r.j.1595@gmail.com>
2021-04-10 18:03:31 -0300

    correciones sobre los bucles

--- TP2_1_1.cpp
+++ TP2_1_1.cpp
@@ -2,17 +2,29 @@
 #define N 4
 #define M 5
 
-void main(){
+int main(){
     int f,c;
     double mt[N][M];
 
     for(f = 0;f<N; f++){
 
-        for(f = 0;f<N; f++){
+        std::cout << "Ingrese los valores para la fila " << f+1 << "\n";
+        for(c = 0;c<M; c++){
     
-            std::cout << mt[f][c];
+            std::cout << "Columna " << c+1 << ": ";
+            std::cin >> mt[f][c];
         }
-        std::cout << “\n”;
     }
+
+    for(f = 0;f<N; f++){
+
+        for(c = 0;c<M; c++){
+    
+            std::cout << mt[f][c] << "  ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
 
 }
